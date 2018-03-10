895f82ef08a328426e76397a5c8da21e0120798c
trossides <theorossides@gmail.com>
2018-03-10 16:32:14 +0930

    Add project files.

---
+++ TheoWebsite/envTheoWebsite/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\rtheo\Source\Repos\TheoWebsite\TheoWebsite\envTheoWebsite\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
