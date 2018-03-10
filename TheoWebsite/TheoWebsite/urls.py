d2231f870146e2cada6b57104e372b3191c3b414
trossides <theorossides@gmail.com>
2018-03-10 16:38:55 +0930

    url update week 2

--- TheoWebsite/TheoWebsite/urls.py
+++ TheoWebsite/TheoWebsite/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
-    # url(r'^$', hello),
+    url(r'^$', hello),
 ]
