0d57ec41f58fe3c516da0ddac2d6b9bd6ba8c1ce
trossides <theorossides@gmail.com>
2018-04-11 11:19:09 +0930

    Week 4

--- week3site/week3_app/models.py
+++ week3site/week3_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    customer_number = models.CharField(max_length=6, help_text='Unique part number for reordering.')
+    address = models.CharField(max_length=200, help_text='Postal address of customer.')
+    phone = models.CharField(max_length=15, help_text='Daytime contact details of customer.')
+    email = models.EmailField(blank=True)
+
+    def __str__(self):
+        return self.name
