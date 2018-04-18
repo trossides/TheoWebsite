205b4b62da5348c14a0eaa741eae96576713dd89
trossides <theorossides@gmail.com>
2018-04-18 10:00:12 +0930

    week 7 take 1

--- week3site/week3_app/admin.py
+++ week3site/week3_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from week3_app.models import Author
+from week3_app.models import Book
+from week3_app.models import Publisher
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Publisher)
--- week3site/week3_app/models.py
+++ week3site/week3_app/models.py
@@ -1,11 +1,31 @@
 from django.db import models
 
 # Create your models here.
-class Customer(models.Model):
-    customer_number = models.CharField(max_length=6, help_text='Unique part number for reordering.')
-    address = models.CharField(max_length=200, help_text='Postal address of customer.')
-    phone = models.CharField(max_length=15, help_text='Daytime contact details of customer.')
-    email = models.EmailField(blank=True)
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=30)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
 
     def __str__(self):
         return self.name
+
+class Author(models.Model):
+   first_name = models.CharField(max_length=30)
+   last_name = models.CharField(max_length=40)
+   email = models.EmailField()
+
+   def __str__(self):
+      return u'%s %s' % (self.first_name, self.last_name)
+
+class Book(models.Model):
+   title = models.CharField(max_length=100)
+   authors = models.ManyToManyField(Author)
+   publisher = models.ForeignKey(Publisher)
+   publication_date = models.DateField()
+
+   def __str__(self):
+      return self.title
